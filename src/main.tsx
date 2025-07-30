ea49f994cc4e8485d9e95e519e8cc67bb353c59a
Jeremy Johnson <jeremy@imtc.gatech.edu>
2025-07-30 09:08:15 -0400

    Replace Authenticator w/ signInWithRedirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import { Authenticator } from '@aws-amplify/ui-react';
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
+import { signInWithRedirect, getCurrentUser, signOut } from "@aws-amplify/auth";
 
 Amplify.configure(outputs);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+function Root() {
+    const [user, setUser] = useState<any>(null);
+    const [loading, setLoading] = useState(true);
 
-    <React.StrictMode>
-        <Authenticator>
+    useEffect(() => {
+        getCurrentUser()
+            .then(setUser)
+            .catch(() => setUser(null))
+            .finally(() => setLoading(false));
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+
+    if (!user) {
+        return (
+            <button onClick={() => signInWithRedirect({provider: {custom: 'GeorgiaTech'}})}>Sign in</button>
+        );
+    }
+
+    return (
+        <>
             <App />
-        </Authenticator>
+            <button onClick={() => signOut()}>Sign out</button>
+        </>
+    );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <Root />
     </React.StrictMode>
 );
